613bb2301d0db24ab0ea643d36c0cbeadc535270
incoronado <mike.larson@gmail.com>
2017-02-21 21:54:41 -0800

    Initial Commit

---
+++ songs.cmd
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\Housebot\config\scripts\id3" -2 -q "Title  : %%t\nArtist : %%a\nAlbum  : %%l\nTrack  : %%n\nGenre  : %%g\nYear   : %%y\nFile   : %%_p%%_f\nCount  : %%X\n" -R "d:\media\Music\*.mp3" > c:\Progra~2\Housebot\Config\Scripts\songs.tmp

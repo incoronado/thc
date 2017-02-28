0e02ee94ab84d8bf20398c1fe0adeefc36c77b92
incoronado <mike.larson@gmail.com>
2017-02-27 20:09:50 -0800

    added pscp

--- build_housebot.ps1
+++ build_housebot.ps1
@@ -3,15 +3,6 @@
 ### Author:         Mike Larson
 ### Description:    Primary Build Script for Housebot
 ###################################################################### 
-$username = "mike"
 $password = "KKy68a?*"
-
-net use \\home-pc $password /USER:$username 
-
-$hb_destination= "\\home-pc\Config\"
-$git_source  = "Scripts\"
-$logfile = $git_source + "robocopyLogFile.txt"
-$switches = ("/E", "/S", "/R:0")
-$roboCopyString = "*.vbs"
-robocopy $git_source $hb_destination"bak" $roboCopyString $switches
+pscp -pw $password -i c:\users\mike\.ssh\id_rsa.pub system.vbs mike@home-pc:"/Program Files (x86)/housebot/config/scripts/"
 

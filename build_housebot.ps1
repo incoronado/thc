e4774d1cd01f546d0cca371e73cb69012dc1eec3
incoronado <mike.larson@gmail.com>
2017-02-27 20:17:27 -0800

    a

--- build_housebot.ps1
+++ build_housebot.ps1
@@ -4,5 +4,5 @@
 ### Description:    Primary Build Script for Housebot
 ###################################################################### 
 $password = "KKy68a?*"
-pscp -pw $password -i c:\users\mike\.ssh\id_rsa.pub Scripts/system.vbs mike@home-pc:"/Program Files (x86)/housebot/config/scripts/"
+pscp -pw KKy68a?* -i c:\users\mike\.ssh\id_rsa.pub Scripts/system.vbs mike@home-pc:"/Program Files (x86)/housebot/config/scripts/"
 

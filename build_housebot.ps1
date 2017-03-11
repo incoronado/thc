4aae2da121bf4aa426a7df4bbfaa4a8cee2cc16c
incoronado <mike.larson@gmail.com>
2017-03-11 12:15:39 -0800

    a

--- build_housebot.ps1
+++ build_housebot.ps1
@@ -3,12 +3,22 @@
 ### Author:         Mike Larson
 ### Description:    Primary Build Script for Housebot
 ###################################################################### 
-# Copy HB Scripts a
+Stop-Process -processname HousebotServer.exe
+
+# Copy HB Scripts
 $source="Scripts"
-$dest="c:\Program Files (x86)\Housebot\Config\Temp"
+$dest="c:\Program Files (x86)\Housebot\Config\Scripts
 
 $what = @("/COPYALL","/B","/MIR")
 $options = @("/R:0","/W:0","/NFL","/NDL")
+
+$cmdArgs = @("$source","$dest",$what,$options)
+robocopy @cmdArgs
+If($?){exit 0}
+
+$source="HBData\HBData.mdb"
+$dest="c:\Program Files (x86)\Housebot\Config\temp\HBData.mdb"
+
 
 $cmdArgs = @("$source","$dest",$what,$options)
 robocopy @cmdArgs

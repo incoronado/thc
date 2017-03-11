c487df3c9b989f85658a7ef333140e9198dca7b3
incoronado <mike.larson@gmail.com>
2017-03-11 13:50:36 -0800

    Added THemes

--- build_housebot.ps1
+++ build_housebot.ps1
@@ -4,6 +4,7 @@
 ### Description:    Primary Build Script for Housebot
 ###################################################################### 
 Stop-Process -processname "HouseBotServer" -Force
+Remove-Item "C:\Program Files (x86)\Housebot\Config\HBData.ldb"
 
 # Copy HB Scripts
 $source="Scripts"
@@ -15,6 +16,18 @@ $options = @("/R:0","/W:0","/NFL","/NDL")
 $cmdArgs = @("$source","$dest",$what,$options)
 robocopy @cmdArgs
 
+$source="Themes"
+$dest="c:\Program Files (x86)\Housebot\Config\Themes"
+
+$what = @("/COPYALL","/B","/MIR")
+$options = @("/R:0","/W:0","/NFL","/NDL")
+
+$cmdArgs = @("$source","$dest",$what,$options)
+robocopy @cmdArgs
+
+
+
+
 $source="HBData"
 $dest="c:\Program Files (x86)\Housebot\Config"
 $filename="HBData.mdb"
@@ -22,3 +35,5 @@ $filename="HBData.mdb"
 $cmdArgs = @("$source","$dest",$filename,$options)
 robocopy @cmdArgs
 If($?){exit 0}
+
+& "C:\Program Files (x86)\Housebot\HouseBotServer.exe"
